d02a6f69e1105773bb8e7f7568f28a1a2b86d3b6
Gussafii <154120123+Gussafii@users.noreply.github.com>
2023-12-18 13:21:20 +0700

    Add files via upload

---
+++ _sidenav.php
@@ -0,0 +1,31 @@
+<div class="main-sidebar sidebar-style-2">
+  <aside id="sidebar-wrapper">
+    <div class="sidebar-brand">
+      <a href="index.php">
+        <img src="https://www.perhutani.co.id/wp-content/themes/perhutani2022/assets/images/logo.png" alt="logo" width="150">
+      </a>
+    </div>
+    <div class="sidebar-brand sidebar-brand-sm">
+      <a href="index.php">EF</a>
+    </div>
+    <ul class="sidebar-menu">
+      <li class="menu-header">Hanya Admin</li>
+      <li><a class="nav-link" href="../"><i class="fas fa-fire"></i> <span>Home</span></a></li>
+      <li class="menu-header">Main Feature</li>
+      <li class="dropdown">
+        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Pegawai</span></a>
+        <ul class="dropdown-menu">
+          <li><a class="nav-link" href="../dosen/index.php">List</a></li>
+          <li><a class="nav-link" href="../dosen/create.php">Tambah Data</a></li>
+        </ul>
+      </li>
+      <li class="dropdown">
+        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Subscribers</span></a>
+        <ul class="dropdown-menu">
+          <li><a class="nav-link" href="../subscribe/index.php">List</a></li>
+          <li><a class="nav-link" href="../subscribe/create.php">Tambah Data</a></li>
+        </ul>
+      </li>
+    </ul>
+  </aside>
+</div>
